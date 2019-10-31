f375d2e0e18751a926ae79381e9cf9250996488f
vinay <vinay.d@teamdatalogic.com>
2019-10-31 08:04:57 +0000

    ui rework

--- vue-expenses-client/src/views/Overview.vue
+++ vue-expenses-client/src/views/Overview.vue
@@ -1,119 +1,159 @@
 <template>
-  <div class="d-flex" style=" height: 100%">
-    <div style="width: 75%;" class="grey lighten-4 pa-4">
-      <v-layout row justify-start align-end pl-8>
-        <!-- <v-icon medium color="deep-orange">shopping_basket</v-icon> -->
-        <div class="ml-2 title-1 blue--text">Expenses</div>
+  <div>
+    <h1 class="headline blue--text text--lighten-1">Overview</h1>
+    <v-container>
+      <v-layout row justify-space-between>
+        <v-flex xs12 md5>
+          <v-card class="pa-3 mr-2 mr-2" raised>
+            <div class="body-1 pa-3 grey--text text--darken-2 text-capitalize">Add New Expense</div>
+            <v-divider></v-divider>
+            <v-form v-model="valid">
+              <v-container>
+                <v-textarea
+                  v-model="description"
+                  :rules="nameRules"
+                  label="Description"
+                  auto-grow="true"
+                  required
+                  clearable
+                  class="ma-0 pa-0 form-label"
+                  dense
+                ></v-textarea>
+                <v-row>
+                  <v-col cols="12" md="6" class="py-0 ma-0">
+                    <v-select
+                      :items="items"
+                      label="Category"
+                      clearable
+                      class="ma-0 pa-0 form-label"
+                      dense
+                      offset-x
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="12" md="6" class="py-0 ma-0">
+                    <v-menu v-model="dateMenu" :close-on-content-click="false" max-width="290">
+                      <template v-slot:activator="{ on }">
+                        <v-text-field
+                          v-model="date"
+                          label="Date"
+                          readonly
+                          clearable
+                          v-on="on"
+                          class="ma-0 pa-0 form-label"
+                          dense
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker v-model="date" @change="dateMenu = false"></v-date-picker>
+                    </v-menu>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12" md="6" class="py-0 ma-0">
+                    <v-select
+                      :items="items"
+                      label="Type"
+                      clearable
+                      class="ma-0 pa-0 form-label"
+                      dense
+                      offset-x
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="12" md="6" class="py-0 ma-0">
+                    <v-text-field
+                      v-model="amount"
+                      :rules="nameRules"
+                      label="Amount"
+                      required
+                      clearable
+                      class="ma-0 pa-0 form-label"
+                      dense
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row class="justify-end">
+                  <v-btn outlined rounded class="blue--text">Submit</v-btn>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 md5>
+          <v-card class="pa-3 mr-2" outlined height="100%">
+            <div class="body-1 pa-3 grey--text text--darken-2 text-capitalize">Monthly Budget</div>
+            <v-divider></v-divider>
+            <DoughnutChart :styles="graphsHeight1" />
+            <v-container pb-0 pt-10>
+              <v-layout row justify-space-around>
+                <v-card flat max-width="200">
+                  <div class="caption">Monthly Limit</div>
+                  <span class="subtitle-2">35000</span>
+                </v-card>
+
+                <v-card flat max-width="200">
+                  <div class="caption">Spent Amount</div>
+                  <span class="subtitle-2">20000</span>
+                </v-card>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+        <v-flex
+          xs12
+          md2
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis deleniti sapiente labore placeat aut mollitia. Aliquam officiis iusto qui pariatur, ducimus iste eveniet possimus ipsam error, suscipit deserunt dolor!</v-flex>
+        <v-flex xs12 md12>
+          <v-container>
+            <v-layout row wrap>
+              <v-flex xs12 md10>
+                <v-card class="pa-3 mr-2" outlined>
+                  <div class="body-1 pa-3 grey--text text--darken-2 text-capitalize">Breakdown</div>
+                  <v-divider></v-divider>
+                  <v-container>
+                    <v-layout row wrap>
+                      <v-flex xs12 md6>
+                        <BarChart :styles="graphsHeight" />
+                      </v-flex>
+                      <v-flex xs12 md6>
+                        <PieChart :styles="graphsHeight2" />
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
       </v-layout>
-      <v-container fluid>
-        <v-row>
-          <v-col class="elevation-0 pl-0">
-            <v-row justify="center">
-              <v-card flat tile class="rounded-card">
-                <div class="d-flex">
-                  <BarChart :styles="graphsHeight" />
-                  <div style="width: 40%; border-left: 0.5px dashed lightgray">
-                    <DoughnutChart :styles="graphsHeight1" />
-                    <div class="d-flex mt-2">
-                      <v-list-item-content class="pa-3 mt-3 ml-10">
-                        <v-list-item-title class="subtitle-2">35,000</v-list-item-title>
-                        <v-list-item-subtitle class="caption">Monthly Limit</v-list-item-subtitle>
-                      </v-list-item-content>
-                      <v-list-item-content class="pa-3 mt-3 ml-0">
-                        <v-list-item-title class="subtitle-2">30,000</v-list-item-title>
-                        <v-list-item-subtitle class="caption">Spent</v-list-item-subtitle>
-                      </v-list-item-content>
-                    </div>
-                  </div>
-                </div>
-              </v-card>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-container>
-      <div class="pl-3 gray--text" left>
-        <span class="ml-2 title-1">Categories With Biggest Expenses</span>
-      </div>
-    </div>
-    <div style="width: 25%;" class="white pl-3 pb-3">
-      <v-layout row justify-start align-baseline pl-6 mt-4>
-        <div class="ml-2 title-2 grey--text">Categories</div>
-      </v-layout>
-      <v-container fluid class="pl-3">
-        <v-card v-for="link in links" :key="link.text" flat tile class="elevation-0">
-          <v-row style="align-items: end">
-            <v-col class="d-flex ml-4">
-              <v-row>
-                <v-icon medium left :color="link.color">{{link.icon}}</v-icon>
-              </v-row>
-              <v-row class="ml-4">
-                <span class="body-2 font-weight-medium">{{link.text}}</span>
-                <span style="font-size: 0.6rem; ">{{link.lastExpenseDate}}</span>
-              </v-row>
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col>
-              <span class="body-2">{{link.value}}</span>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-container>
-    </div>
+    </v-container>
   </div>
 </template>
-
 <script>
-import BarChart from "@/components/BarChart";
 import DoughnutChart from "@/components/DoughnutChart";
+import BarChart from "@/components/BarChart";
+import PieChart from "@/components/PieChart";
 
 export default {
-  components: { BarChart, DoughnutChart },
+  components: { DoughnutChart, BarChart, PieChart },
   data() {
     return {
+      dateMenu: false,
       graphsHeight: {
         height: "280px"
       },
       graphsHeight1: {
-        height: "200px"
+        height: "215px"
       },
-      links: [
-        {
-          icon: "insert_chart",
-          text: "General",
-          lastExpenseDate: "20/10/2019",
-          value: "4,000",
-          color: "orange"
-        },
-        {
-          icon: "shopping_cart",
-          text: "Shopping",
-          lastExpenseDate: "20/10/2019",
-          value: "200",
-          color: "blue"
-        },
-        {
-          icon: "hotel",
-          text: "Travel",
-          lastExpenseDate: "20/10/2019",
-          value: "3,129",
-          color: "green"
-        },
-        {
-          icon: "lightbulb_outline",
-          text: "Utilities",
-          lastExpenseDate: "20/10/2019",
-          value: "300",
-          color: "purple"
-        }
-      ]
+      graphsHeight2: {
+        height: "275px"
+      }
     };
   }
 };
 </script>
 
 <style>
-.main-span:before {
-  content: "\A";
-  white-space: pre;
+.form-label label[for] {
+  height: 20px;
+  font-size: 10pt;
 }
 </style>
