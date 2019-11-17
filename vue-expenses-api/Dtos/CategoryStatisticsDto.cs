bc7bf93b578aba7a73ee955014581bd654cac15e
vinay <167894+simplyvinay@users.noreply.github.com>
2019-11-17 04:45:21 +0000

    adding stats controller

---
+++ vue-expenses-api/Dtos/CategoryStatisticsDto.cs
@@ -0,0 +1,26 @@
+﻿namespace vue_expenses_api.Dtos
+{
+    public class CategoryStatisticsDto
+    {
+        protected CategoryStatisticsDto()
+        {
+        }
+
+        public CategoryStatisticsDto(
+            string name,
+            decimal budget,
+            decimal spent,
+            string colour)
+        {
+            Name = name;
+            Budget = budget;
+            Spent = spent;
+            Colour = colour;
+        }
+
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public decimal Spent { get; set; }
+        public string Colour { get; set; }
+    }
+}
---
+++ vue-expenses-api/Features/Statistics/StatisticsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using vue_expenses_api.Dtos;
+
+namespace vue_expenses_api.Features.Statistics
+{
+    [Route("statistics")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class StatisticsController
+    {
+        private readonly IMediator _mediator;
+
+        public StatisticsController(
+            IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<List<CategoryStatisticsDto>> GetCurrentMonthCategoryBreakdown()
+        {
+            return await _mediator.Send(new StatisticsList.Query(StatisticsList.EnumCategoryBreakdownBy.Month));
+        }
+
+        [HttpGet]
+        public async Task<List<CategoryStatisticsDto>> GetCurrentYearCategoryBreakdown()
+        {
+            return await _mediator.Send(new StatisticsList.Query(StatisticsList.EnumCategoryBreakdownBy.Year));
+        }
+
+    }
+}
