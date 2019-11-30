f4d233a7685737c367da93825ee0e741bdeeec72
vinay <vinay.d@teamdatalogic.com>
2019-11-30 15:46:19 +0000

    some view fixes

--- vue-expenses-client/src/components/ExpenseTypeStats.vue
+++ vue-expenses-client/src/components/ExpenseTypeStats.vue
@@ -29,9 +29,9 @@
           <v-divider></v-divider>
           <v-container>
             <v-layout row wrap>
-              <v-flex xs12 md6 mt-5>
+              <v-flex xs12 md6 mt-4>
                 <v-data-table
-                  height="340px"
+                  height="280px"
                   :headers="headers"
                   :items="typesBreakdown"
                   sort-by="name"
