0bc1ff3fc4ec72661305b80ad381783864cce6b3
vinay <vinay.d@teamdatalogic.com>
2019-12-01 15:42:32 +0000

    changes to be able load vue client app from server

--- vue-expenses-client/vue.config.js
+++ vue-expenses-client/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  productionSourceMap: false,
+  outputDir: path.resolve(__dirname, "../vue-expenses-api/wwwroot"),
+  assetsDir: "../../vue-expenses-api/wwwroot/static"
 }
